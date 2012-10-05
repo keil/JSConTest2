28789a530cfa207734aef8a2699785e14b8c96a6
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2012-10-05 09:39:50 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/trunk@22769 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- __test_contractset.js
+++ __test_contractset.js
@@ -3,26 +3,26 @@ parser = new __ContractParser();
 
 function test(string, name0, name1) {
 		contract = parser.parse(string);
-		var result = contract.readable(name1);
-		__sysout("[" + contract.toString() + "] " + name0 + ": " + result.readable + "/ " + (result.contracts!="" ? result.contracts : "{}"));
-		var result = result.contracts.readable(name1);
-		__sysout("[" + result.contracts.toString() + "] " + name1 + ": " + result.readable + "/ " + (result.contracts!="" ? result.contracts : "{}"));
+		var result1 = contract.readable(name1);
+		__sysout("[" + contract.toString() + "] " + name0 + ": " + result1.readable + "/ " + (result1.contracts.toString()!="" ? result1.contracts : "{}"));
+		var result2 = result1.contracts.readable(name1);
+		__sysout("[" + result1.contracts.toString() + "] " + name1 + ": " + result2.readable + "/ " + (result2.contracts.toString()!="" ? result2.contracts : "{}"));
 }
 
 
 
-__sysout("# TEST 1 #");
+__sysout("\n# TEST 1 #");
 test("@.b.c", "a", "b");
 test("@.b*.c", "b", "b");
 test("@", "a", "");
 
-__sysout("# TEST 2 #");
+__sysout("\n# TEST 2 #");
 test("?.b.c", "a", "b");
 test("?.b*.c", "b", "b");
 test("?", "1", "b");
 test("?", "1", "");
 
-__sysout("# TEST 3 #");
+__sysout("\n# TEST 3 #");
 //test("[a-z].b.c", "a", "b");
 test("a.b.c", "a", "b");
 //test("[a-z].b.c", "b", "b");
@@ -31,7 +31,7 @@ test("a.b.c", "b", "a");
 test("a", "a", "a");
 test("b", "b", "b");
 
-__sysout("# TEST 4 #");
+__sysout("\n# TEST 4 #");
 //test("[a-z].b.c", "a", "b");
 test("a*.b.c", "a", "b");
 //test("[a-z].b.c", "b", "b");
@@ -40,7 +40,7 @@ test("a*.b.c", "b", "b");
 test("a*", "a", "a");
 test("b*", "b", "a");
 
-__sysout("# TEST 5 #");
+__sysout("\n# TEST 5 #");
 //test("[a-z].b.c", "a", "b");
 test("a?.b.c", "a", "a");
 //test("[a-z].b.c", "b", "b");
@@ -49,7 +49,7 @@ test("a?.b.c", "b", "b");
 test("a?", "a", "b");
 test("b?", "b", "b");
 
-__sysout("# TEST 6 #");
+__sysout("\n# TEST 6 #");
 test("(a|b).b.c", "a", "b");
 test("(a|b).b.c", "b", "b");
 test("(a|b).b.c", "c", "b");
@@ -57,7 +57,7 @@ test("(a|b)", "a", "a");
 test("(a|b)", "b", "b");
 test("(a|b)", "c", "");
 
-__sysout("# TEST 7 #");
+__sysout("\n# TEST 7 #");
 test("(a|b)*.b.c", "a", "a");
 test("(a|b)*.b.c", "b", "a");
 test("(a|b)*.b.c", "a", "b");
@@ -66,7 +66,7 @@ test("(a|b)*", "b", "b");
 test("(a|b)*", "c", "c");
 
 
-__sysout("# TEST 8 #");
+__sysout("\n# TEST 8 #");
 test("(a|b)?.b.c", "a", "a");
 test("(a|b)?.b.c", "b", "a");
 test("(a|b)?.b.c", "a", "b");
@@ -74,6 +74,6 @@ test("(a|b)?", "a", "a");
 test("(a|b)?", "b", "b");
 test("(a|b)?", "c", "c");
 
-__sysout("# TEST 9 #");
+__sysout("\n# TEST 9 #");
 //test("(a|b*).b.c", "bbb", "a");
 
