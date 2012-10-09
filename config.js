de4088dceab3a1d9bcf5665098d6d1e230520d2f
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2012-10-09 12:39:31 +0000

    new version number
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/trunk@22775 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // http://proglang.informatik.uni-freiburg.de/
 // Author: Matthias Keil
 // http://www.informatik.uni-freiburg.de/~keilr/
-// Version: 0.20.2
+// Version: 0.21.0
 //////////////////////////////////////////////////
 
 
