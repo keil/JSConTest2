6165af57eb54fd252a86f27e8f9cb2cae101c4a2
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2012-11-13 14:57:56 +0000

    add new starting file
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/trunk@22919 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ apc.js
@@ -0,0 +1,15 @@
+//////////////////////////////////////////////////
+// JavaScript Reflection API
+//  for Access Permission Contracts
+// (c) University of Freiburg
+// http://proglang.informatik.uni-freiburg.de/
+// Author: Matthias Keil
+// http://www.informatik.uni-freiburg.de/~keilr/
+// Version: 2.00
+//////////////////////////////////////////////////
+
+var __APC = (function() {
+
+		return {}
+
+})();
