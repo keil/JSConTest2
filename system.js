96787c6aa5c5ee0370f38c85330bba5d62436ae4
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2012-11-16 13:29:45 +0000

    add html output
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/trunk@22949 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- system.js
+++ system.js
@@ -65,3 +65,78 @@ function __look() {
 		__APC.Access.dump();
 		__APC.Violation.dump();
 }
+
+/* Check function
+ * append output to DOM node
+ * @param domNode DOM node
+ */
+function __appendLook(domNode) {
+		var headingAP = document.createElement("h2"),
+			headingV = document.createElement("h2"),
+			divChildAP = document.createElement("div"),
+			divChildV = document.createElement("div");
+
+		headingAP.innerHTML = "EVALUATION of ACCESS";
+		headingV.innerHTML = "EVALUATION of VIOLATIONS";
+
+		domNode.appendChild(headingAP);
+		domNode.appendChild(divChildAP);
+		domNode.appendChild(headingV);
+		domNode.appendChild(divChildV);
+
+		function mkDtTag(v) {
+				tmpDt = document.createElement("dt");
+				tmpDt.innerHTML = v.type.toString();
+				tmpDt.style.cssFloat = "left";
+
+				switch(v.type) {
+
+						case __APC.Access.Type.READ:
+								tmpDt.style.width = "50px";	
+								tmpDt.style.background = "#8793a1";
+
+								break;
+						case __APC.Access.Type.WRITE:
+								tmpDt.style.width = "50px";	
+								tmpDt.style.background = "#919191";
+
+								break;
+						case __APC.Violation.Type.READ:
+								tmpDt.style.width = "100px";	
+								tmpDt.style.background = "#5f9be3";
+
+								break;
+						case __APC.Violation.Type.WRITE:
+								tmpDt.style.width = "100px";	
+								tmpDt.style.background = "#849be1";
+
+								break;
+				}
+
+				tmpDt.style.padding = "2px";
+				tmpDt.style.margin = "0 1em 0 0";
+				return tmpDt;
+		}
+
+		function mkDdTag(v) {
+				tmpDd = document.createElement("dd");
+				tmpDd.innerHTML = v.path.toString();
+				tmpDd.style.padding = "2px";
+				return tmpDd;
+		}
+
+		dlAP = document.createElement("dl");
+		__APC.Access.Logger.foreach(function(v) {
+				dlAP.appendChild(mkDtTag(v));
+				dlAP.appendChild(mkDdTag(v));
+		});
+		divChildAP.appendChild(dlAP);
+
+		dlV = document.createElement("dl");
+		__APC.Violation.Logger.foreach(function(v) {
+				dlV.appendChild(mkDtTag(v));
+				dlV.appendChild(mkDdTag(v));
+
+		});
+		divChildV.appendChild(dlV);
+}
