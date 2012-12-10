166385d77c2390efa6a0b2d04c0ee55725be86a7
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2012-12-10 15:15:05 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/trunk@23020 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- bootstrap_test.js
+++ bootstrap_test.js
@@ -62,38 +62,57 @@ load("permit.js")
 // TEST
 //////////////////////////////////////////////////
 
+var __start = new Date().getTime();
+
 /* Test Parser **/ // (TESTED)
-load("__test/__test_parser.js");
+//load("__test/__test_parser.js");
 
 /* Test Path **/ // (TESTED)
-load("__test/__test_path.js");
-load("__test/__test_path2.js");
+//load("__test/__test_path.js");
+//load("__test/__test_path2.js");
 
 /** Test Contract **/ // (TESTED)
-load("__test/__test_contract.js");
+//load("__test/__test_contract.js");
 
 /** Test Contract (reduced) **/ // (TESTED)
-load("__test/__test_contract_reduce.js");
+//load("__test/__test_contract_reduce.js");
 
 /* Test Contract (derive) **/  // (TESTED)
-load("__test/__test_contract_read.js");
-load("__test/__test_contract_write.js");
-load("__test/__test_set_read.js");
-load("__test/__test_set_write.js");
+//load("__test/__test_contract_read.js");
+//load("__test/__test_contract_write.js");
+//load("__test/__test_set_read.js");
+//load("__test/__test_set_write.js");
 
 
 /* Test Permit **/  // (TESTED)
-load("__test/__test_permit.js");
-load("__test/__test_permit2.js");
-load("__test/__test_permit3.js");
-load("__test/__test_permit4.js");
-load("__test/__test_permit5.js");
-load("__test/__test_permit6.js");
-load("__test/__test_permit7.js");
+//load("__test/__test_permit.js");
+//load("__test/__test_permit2.js");
+//load("__test/__test_permit3.js");
+//load("__test/__test_permit4.js");
+//load("__test/__test_permit5.js");
+//load("__test/__test_permit6.js");
+//load("__test/__test_permit7.js");
 
 
 /* Test Permit (special) **/ // (TESTED)
-load("__test/__test_scope.js");
-load("__test/__test_scope2.js");
+//load("__test/__test_scope.js");
+//load("__test/__test_scope2.js");
+
+
+/* Test Path (membership) **/ // 
+load("__test/__test_subset.js");
+load("__test/__test_superset.js");
+load("__test/__test_sreduce.js");
+//load("__test/__test_flattening.js");
+
+
+
+
+var __end = new Date().getTime();
+
+__sysout("%% START: " + __start);
+__sysout("%% END: " + __end);
+__sysout("%% TIME: " + (__end - __start));
+
 
 quit();
