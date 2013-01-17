a69e00ceb5eed79a2e4d96c74091830e14232c15
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2013-01-17 09:01:26 +0000

    add testcase
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/trunk@23069 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ __test/__test_contract_subset.js
@@ -0,0 +1,220 @@
+//////////////////////////////////////////////////
+// JavaScript Reflection API
+//  for Access Permission Contracts
+// - TestCase -
+// (c) University of Freiburg
+// http://proglang.informatik.uni-freiburg.de/
+// Author: Matthias Keil
+// http://www.informatik.uni-freiburg.de/~keilr/
+//////////////////////////////////////////////////
+
+function testS(cstring0, cstring1) {
+		// parse contracts
+		contract0 = __APC.Parser.parse(cstring0);
+		// parse contracts
+		contract1 = __APC.Parser.parse(cstring1);
+
+		__sysout("\n##########");
+		__sysout("C0: " + contract0);
+		__sysout("C1: " + contract1);
+		__sysout(contract0 + " <= " + contract1 + ": " + contract0.isSubSetOf(contract1, new __APC.Contract.Containment.Context()));
+		__sysout(contract1 + " >= " + contract0 + ": " + contract1.isSuperSetOf(contract0, new __APC.Contract.Containment.Context()));
+
+
+		// test
+		result0 = assertTrue(contract0 + " <= " + contract1, contract0.isSubSetOf(contract1, new __APC.Contract.Containment.Context()));
+		result1 = assertTrue(contract1 + " >= " + contract0, contract1.isSuperSetOf(contract0, new __APC.Contract.Containment.Context()));
+		__sysout("TEST: " + result0 + " " + result1);
+}
+
+function testN(cstring0, cstring1) {
+		// parse contracts
+		contract0 = __APC.Parser.parse(cstring0);
+		// parse contracts
+		contract1 = __APC.Parser.parse(cstring1);
+
+
+
+		__sysout("\n##########");
+		__sysout("C0: " + contract0);
+		__sysout("C1: " + contract1);
+		__sysout(contract0 + " <= " + contract1 + ": " + contract0.isSubSetOf(contract1, new __APC.Contract.Containment.Context()));
+		__sysout(contract1 + " >= " + contract0 + ": " + contract1.isSuperSetOf(contract0, new __APC.Contract.Containment.Context()));
+
+		// test
+		result0 = assertFalse(contract0 + " <= " + contract1, contract0.isSubSetOf(contract1, new __APC.Contract.Containment.Context()));
+		result1 = assertFalse(contract1 + " >= " + contract0, contract1.isSuperSetOf(contract0, new __APC.Contract.Containment.Context()));
+		__sysout("TEST: " + result0 + " " + result1);
+}
+
+
+// LITERAL TESTS
+
+
+testN("",		"@");
+testS("@",		"@");
+testN("a",		"@");
+testN("/a/",	"@");
+testN("?",		"@");
+
+
+
+testN("",		"a");
+testS("@",		"a");
+testS("a",		"a");
+testN("/a/",	"a");
+testN("?",		"a");
+
+
+
+testN("",		"/a/");
+testS("@",		"/a/");
+testN("a",		"/a/");
+// testS("a",		"/a/"); kann aktuell nicht bestimmt werden.
+testS("/a/",	"/a/");
+testN("?",		"/a/");
+
+
+
+testS("",		"?");
+testS("@",		"?");
+testS("a",		"?");
+testS("/a/",	"?");
+testS("?",		"?");
+
+
+
+testN("",		"@?");
+testS("@",		"@?");
+testN("a",		"@?");
+testN("/a/",	"@?");
+testN("?",		"@?");
+testN("!(a)",	"@?");
+
+
+
+testN("",		"a?");
+testS("@",		"a?");
+testS("a",		"a?");
+testN("/a/",	"a?");
+testN("?",		"a?");
+testN("!(a)",	"a?");
+
+
+
+testN("",		"/a/?");
+testS("@",		"/a/?");
+testN("a",		"/a/?");
+// testS("a",		"/a/"); kann aktuell nicht bestimmt werden.
+testS("/a/",	"/a/?");
+testN("?",		"/a/?");
+testN("!(a)",	"/a/?");
+
+
+
+testS("",		"??");
+testS("@",		"??");
+testS("a",		"??");
+testS("/a/",	"??");
+testS("?",		"??");
+testN("!(a)",	"??");
+
+
+
+
+testN("",		"@*");
+testS("@",		"@*");
+testN("a",		"@*");
+testN("/a/",	"@*");
+testN("?",		"@*");
+testN("!(a)",	"@*");
+
+
+
+testN("",		"a*");
+testS("@",		"a*");
+testS("a",		"a*");
+testN("/a/",	"a*");
+testN("?",		"a*");
+testN("!(a)",	"a*");
+
+
+
+testN("",		"/a/*");
+testS("@",		"/a/*");
+testN("a",		"/a/*");
+// testS("a",		"/a/"); kann aktuell nicht bestimmt werden.
+testS("/a/",	"/a/*");
+testN("?",		"/a/*");
+testN("!(a)",	"/a/*");
+
+
+
+testS("",		"?*");
+testS("@",		"?*");
+testS("a",		"?*");
+testS("/a/",	"?*");
+testS("?",		"?*");
+testS("!(a)",	"?*");
+
+
+
+testS("",		"(a+?)");
+testS("@",		"(a+?)");
+testS("a",		"(a+?)");
+testS("/a/",	"(a+?)");
+testS("?",		"(a+?)");
+testN("!(a)",	"(a+?)");
+
+testN("",		"(a+b)");
+testS("@",		"(a+b)");
+testS("a",		"(a+b)");
+testN("/a/",	"(a+b)");
+testN("?",		"(a+b)");
+testN("!(a)",	"(a+b)");
+
+
+
+testN("",		"(a&?)");
+testS("@",		"(a&?)");
+testS("a",		"(a&?)");
+testN("/a/",	"(a&?)");
+testN("?",		"(a&?)");
+testN("!(a)",	"(a&?)");
+
+testN("",		"(a&b)");
+testS("@",		"(a&b)");
+testN("a",		"(a&b)");
+testN("/a/",	"(a&b)");
+testN("?",		"(a&b)");
+testN("!(a)",	"(a&b)");
+
+
+
+testS("",		"!(a)");
+testS("@",		"!(a)");
+testN("a",		"!(a)");
+testS("/a/",	"!(a)");
+testN("?",		"!(a)");
+testS("!(a)",	"!(a)");
+
+testN("",		"!(?)");
+testS("@",		"!(?)");
+testN("a",		"!(?)");
+testN("/a/",	"!(?)");
+testN("?",		"!(?)");
+testN("!(a)",	"!(?)");
+
+
+
+// SOPER TESTS
+testS("!(?)",	"!(a)");
+testS("@",		"!(@)");
+testS("?",		"!(@)");
+testS("?*",		"!(@)");
+testS("!(a)",	"?*");
+
+
+
+
+
